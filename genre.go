a4d5abf733ce6a3d4ba55f1e924e4859daac6447
Barsanuphe <paixetprosperite@gmail.com>
2016-01-16 00:20:36 +0100

    Split code into 3 files.

---
+++ genre.go
@@ -0,0 +1,26 @@
+package main
+
+import "sort"
+
+type Genre struct {
+	Name    string
+	Folders []string
+}
+
+func (g *Genre) String() string {
+	txt := g.Name + ":\n"
+	for _, artist := range g.Folders {
+		txt += "\t- " + artist + "\n"
+	}
+	return txt
+}
+
+func (g *Genre) HasArtist(artist string) bool {
+	sort.Strings(g.Folders)
+	i := sort.SearchStrings(g.Folders, artist)
+	if i < len(g.Folders) && g.Folders[i] == artist {
+		// fmt.Println("++ Found artist ", artist, "in genre ", g.Name)
+		return true
+	}
+	return false
+}
--- radis.go
+++ radis.go
@@ -1,124 +1,16 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
-	"sort"
 	"time"
 
 	"gopkg.in/yaml.v2"
 )
 
 // TODO : save config with sorted Folders
-
-//----------------
-type Genre struct {
-	Name    string
-	Folders []string
-}
-
-func (g *Genre) String() string {
-	txt := g.Name + ":\n"
-	for _, artist := range g.Folders {
-		txt += "\t- " + artist + "\n"
-	}
-	return txt
-}
-
-func (g *Genre) HasArtist(artist string) bool {
-	sort.Strings(g.Folders)
-	i := sort.SearchStrings(g.Folders, artist)
-	if i < len(g.Folders) && g.Folders[i] == artist {
-		// fmt.Println("++ Found artist ", artist, "in genre ", g.Name)
-		return true
-	}
-	return false
-}
-
-//----------------
-
-// TODO check if we need other unicode classes
-var reAlbum = regexp.MustCompile(`^([\p{L}\d_ ]+) \(([0-9]+)\) ([\p{L}\d_ ]+)(\s\[MP3\])?$`)
-
-type AlbumFolder struct {
-	Root   string
-	Path   string
-	Artist string
-	Year   string
-	Title  string
-	IsMP3  bool
-}
-
-func (a *AlbumFolder) String() (albumName string) {
-	albumName = a.Artist + " (" + a.Year + ") " + a.Title
-	if a.IsMP3 {
-		albumName += " [MP3]"
-	}
-	return
-}
-
-func (a *AlbumFolder) IsAlbum() bool {
-	// TODO isDIR
-	if err := a.ExtractInfo(); err != nil {
-		// fmt.Println(err)
-		return false
-	}
-	return true
-}
-
-func (a *AlbumFolder) ExtractInfo() (err error) {
-	matches := reAlbum.FindStringSubmatch(filepath.Base(a.Path))
-	if len(matches) > 0 {
-		a.Artist = matches[1]
-		a.Year = matches[2]
-		a.Title = matches[3]
-		a.IsMP3 = matches[4] != ""
-	} else {
-		err = errors.New("Not an album!")
-	}
-	return
-}
-
-func (a *AlbumFolder) MoveToNewPath(genre string) (hasMoved bool, err error) {
-	hasMoved = false
-
-	if !a.IsAlbum() {
-		return false, errors.New("Cannot move, not an album.")
-	}
-
-	directoryName := filepath.Base(a.Path)
-	newPath := filepath.Join(a.Root, genre, a.Artist, directoryName)
-	// comparer avec l'ancien
-	if newPath != a.Path {
-		// if different, move folder
-		originalRelative, _ := filepath.Rel(a.Root, a.Path)
-		destRelative, _ := filepath.Rel(a.Root, newPath)
-		fmt.Println("+ "+originalRelative, " -> ", destRelative)
-
-		// TODO create newPath parent if it does not exist
-		newPathParent := filepath.Dir(newPath)
-		if _, err = os.Stat(newPathParent); os.IsNotExist(err) {
-			// newPathParent does not exist, creating
-			err = os.MkdirAll(newPathParent, 0777)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		// move
-		err = os.Rename(a.Path, newPath)
-		if err == nil {
-			hasMoved = true
-		}
-	}
-	return
-}
-
-//----------------
 
 func printConfig(config []Genre) {
 	for _, genre := range config {
@@ -185,7 +77,7 @@ func sortFolders(root string, config []Genre) (err error) {
 					hasMoved, err = af.MoveToNewPath("UNCATEGORIZED")
 				}
 				if hasMoved {
-					movedAlbums ++
+					movedAlbums++
 				}
 			} else {
 				// fmt.Println("++ Skipping, not an album.")
