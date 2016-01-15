d34d77f7987d66b437f535f53ffde88cef4026aa
Barsanuphe <paixetprosperite@gmail.com>
2016-01-15 22:30:24 +0100

    Now it at least runs. Barely.

--- radis.go
+++ radis.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"sort"
+	"strconv"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -30,7 +31,7 @@ func (g *Genre) String() string {
 
 func (g *Genre) HasArtist(artist string) bool {
 	sort.Strings(g.Folders)
-	i := sort.SearchStrings(len(g.Folders), artist)
+	i := sort.SearchStrings(g.Folders, artist)
 	if i < len(g.Folders) && g.Folders[i] == artist {
 		fmt.Println("Found artist ", artist, "in genre ", g.Name)
 		return true
@@ -52,15 +53,19 @@ type AlbumFolder struct {
 }
 
 func (a *AlbumFolder) IsAlbum() bool {
-	// TODO isDIR + successful ExtractInfo
+	// TODO isDIR
+	if err := a.ExtractInfo(); err != nil {
+		return false
+	}
 	return true
 }
 
 func (a *AlbumFolder) ExtractInfo() (err error) {
 	matches := reAlbum.FindStringSubmatch(filepath.Base(a.Path))
-	if matches {
+	fmt.Println(matches)
+	if len(matches) > 0 {
 		a.Artist = matches[1]
-		a.Year = matches[2]
+		a.Year, _ = strconv.Atoi(matches[2])
 		a.Title = matches[3]
 	}
 
@@ -68,7 +73,7 @@ func (a *AlbumFolder) ExtractInfo() (err error) {
 	return
 }
 
-func (a *AlbumFolder) MoveToNewPath(root string, genre string) (err error) {
+func (a *AlbumFolder) MoveToNewPath(genre string) (err error) {
 	if !a.IsAlbum() {
 		fmt.Println("ERRRRRR")
 		// TODO return ERR
@@ -81,13 +86,14 @@ func (a *AlbumFolder) MoveToNewPath(root string, genre string) (err error) {
 	if a.IsMP3 {
 		directoryName += " [MP3]"
 	}
-	newPath := filepath.Join(root, genre, a.Artist, directoryName)
+	newPath := filepath.Join(a.Root, genre, a.Artist, directoryName)
 	// comparer avec l'ancien
 	if newPath != a.Path {
 		fmt.Println(a.Path, " -> ", newPath)
 		// if different, move folder
 		os.Rename(a.Path, newPath)
 	}
+	return
 }
 
 //----------------
@@ -127,66 +133,32 @@ func timeTrack(start time.Time, name string) {
 func sortFolders(root string, config []Genre) (err error) {
 	defer timeTrack(time.Now(), "Scanning files")
 
-	re := regexp.MustCompile(`^([\p{L}\d_ ]+) \(([0-9]+)\) ([\p{L}\d_ ]+)$`)
-	var artist, title string
-	var year int
-
-	err = filepath.Walk(root, func(path string, fileInfo os.FileInfo, _ error) error {
+	err = filepath.Walk(root, func(path string, fileInfo os.FileInfo, _ error) (err error) {
 		relative, _ := filepath.Rel(root, path)
 		fmt.Println("Scanning ", relative)
 
 		if fileInfo.IsDir() {
 			fmt.Println("-> is dir!")
 
-			// TODO use AlbumFolder
-
-			// find dir name
-			directoryName := filepath.Base(relative)
-			// extract artist, year, title, is_mp3
-			matches := re.FindStringSubmatch(directoryName)
-			if matches {
-				artist = matches[1]
-				year = matches[2]
-				title = matches[3]
-
+			af := AlbumFolder{Root: root, Path: path}
+			if af.IsAlbum() {
 				found := false
-				for genre, _ := range config {
+				for _, genre := range config {
 					// if artist is known, it belongs to genre.Name
-					if genre.HasArtist(artist) {
-						// prepare new path genre.Name/artist/directoryName
-						newPath = filepath.Join(root, genre.Name, artist, directoryName)
-						fmt.Println(newPath)
-
-						// compare with current
-						if newPath != path {
-							fmt.Println(path, " -> ", newPath)
-							// if different, move folder
-							os.rename(path, newPath)
-						}
+					if genre.HasArtist(af.Artist) {
+						err = af.MoveToNewPath(genre.Name)
 						found = true
 					}
 				}
-				// if not found, show error.
 				if !found {
-					fmt.Println("!!!! ERR artist not found: ", artist)
-
-					// move to UNCATEGORIZED
-					// prepare new path genre.Name/artist/directoryName
-					newPath = filepath.Join(root, "UNCATEGORIZED", artist, directoryName)
-					fmt.Println(newPath)
-
-					// compare with current
-					if newPath != path {
-						fmt.Println(path, " -> ", newPath)
-						// if different, move folder
-						os.rename(path, newPath)
-					}
+					err = af.MoveToNewPath("UNCATEGORIZED")
 				}
-			} else {
-				fmt.Println("Skipping ", directoryName)
 			}
 
+		} else {
+			fmt.Println("Skipping ", relative)
 		}
+		return
 	})
 	if err != nil {
 		fmt.Printf("Error!")
@@ -200,7 +172,6 @@ func sortFolders(root string, config []Genre) (err error) {
 func main() {
 	fmt.Println("R A D I S\n---------\n")
 	pwd, _ := os.Getwd()
-
 
 	config, err := readConfig(filepath.Join(pwd, "radis.yaml"))
 	if err != nil {
