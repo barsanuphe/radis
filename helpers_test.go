54d6f73d116b7b5406c8a51a5b435a8447961544
Barsanuphe <paixetprosperite@gmail.com>
2016-01-19 21:39:33 +0100

    Added more tests and test cases, corrected album detection regexp.

--- helpers_test.go
+++ helpers_test.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+func TestIsEmpty(t *testing.T) {
+	current, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Could not get current directory!")
+	}
+	isEmpty, err := IsEmpty(current)
+	if isEmpty || err != nil {
+		t.Errorf("Current directory is not empty!")
+	}
+}
+
+var directoryDoesNotExistError = errors.New("Directory /ddsdcisj does not exist!!!")
+var goPath = os.Getenv("GOPATH")
 
 var testPaths = []struct {
 	path         string
@@ -11,8 +27,9 @@ var testPaths = []struct {
 	err          error
 }{
 	{"/tmp", "/tmp", nil},
-	{"/ddsdcisj", "/ddsdcisj", errors.New("Directory /ddsdcisj does not exist!!!")},
-	// TODO more avec cas en relatif
+	{"/ddsdcisj", "/ddsdcisj", directoryDoesNotExistError},
+	{"/ddsdcisj", "/ddsdcisj", directoryDoesNotExistError},
+	{"relative", filepath.Join(goPath, "src/github.com/barsanuphe/radis/relative"), errors.New("Directory relative does not exist!!!")},
 }
 
 func TestGetExistingPath(t *testing.T) {
@@ -23,5 +40,17 @@ func TestGetExistingPath(t *testing.T) {
 		} else if err != nil && tp.err != nil && err.Error() != tp.err.Error() {
 			t.Errorf("GetExistingPath(%s) returned err %s, expected %s", tp.path, err.Error(), tp.err.Error())
 		}
+	}
+}
+
+func TestHasNonFlacFiles(t *testing.T) {
+	// TODO create fake directory with flac files
+	current, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Could not get current directory!")
+	}
+	hasNonFlac, err := HasNonFlacFiles(current)
+	if !hasNonFlac || err != nil {
+		t.Errorf("Current directory contains forbidden files!")
 	}
 }
